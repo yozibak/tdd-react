95535c48de72692971396fa59a85c3dbb2b56fa8
Katsumi Yoshida <yozibak@gmail.com>
2022-08-28 14:57:39 +0900

    change exercises

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App', () => {
-  render(<App />);
-  expect(screen.getByText(/Product Review Form/)).toBeInTheDocument()
-});
+export {}
